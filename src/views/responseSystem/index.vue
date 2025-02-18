7a6044e9fb34bb0eda2345a68e85f3534f497650
343347@libinghai <1395346178@qq.com>
2025-02-18 15:33:44 +0800

    feat: :sparkles: 响应系统:避免使用硬编码

--- src/views/responseSystem/index.vue
+++ src/views/responseSystem/index.vue
@@ -1,16 +1,23 @@
 <script setup lang="ts">
+type anyFnType = (...arg: any) => any;
 onMounted(() => {
   // 需要被代理的对象
   const data: Record<string | symbol, any> = {
     text: '这是初始文本.',
   };
+  // 用来存储当前正在执行的函数,避免使用硬编码 setPContent
+  let activeEffect: anyFnType;
+  function effect(fn: anyFnType) {
+    activeEffect = fn;
+    fn();
+  }
   // 用来存储代理对象发生改变时需要执行的函数
-  const bubble: Set<(...arg: any) => any> = new Set();
+  const bubble: Set<anyFnType> = new Set();
   // 代理对象
   const proxyObj = new Proxy(data, {
     get(target, key) {
       // 当代理对象被访问时,将需要执行的函数存入bubble
-      bubble.add(setPContent);
+      bubble.add(activeEffect);
       return target[key];
     },
     set(target, key, value) {
@@ -27,7 +34,7 @@ onMounted(() => {
   function setPContent() {
     p!.textContent = proxyObj.text;
   }
-  setPContent();
+  effect(setPContent);
   setTimeout(() => {
     proxyObj.text = '这是修改后的文本';
   }, 3000);
