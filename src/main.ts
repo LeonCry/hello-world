fbcf5a3d9a02bef51108745c2c81188288ba8106
343347@libinghai <1395346178@qq.com>
2025-02-19 13:05:35 +0800

    feat: :sparkles: 响应系统:分支切换问题

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,3 @@ import './styles/main.css';
 
 const app = createApp(App).use(router).use(createPinia().use(piniaPluginPersistedstate));
 app.mount('#app');
-app.config.errorHandler = (err, instance, info) => {
-  console.log('error:', err, instance, info);
-};
--- src/views/responseSystem/index.vue
+++ src/views/responseSystem/index.vue
@@ -1,22 +1,56 @@
-<!-- 写在前面: 当前修改proxyObj.text将会触发响应系统,但是如果修改proxyObj.file,那么响应系统仍旧会更新,
-     但是更新的是text,而不是file.这是因为bubble内并没有存储和file相关的函数.这是本次提交需要解决的问题.
+<!-- 写在前面: 本次修改涉及到的是分支切换（三元表达式）时，会产生多余的副作用函数问题，
+多余副作用问题:
+  如果 isShowText 初始值是true, p!.textContent = proxyObj.isShowText ? proxyObj.text : 'nothing';
+  当设置isShowText的值为false之后，p!.textContent的值就是nothing了，这个时候再修改proxyObj.text，虽然界面没有更新，
+  但是副作用函数仍旧会执行，这就是多余副作用问题。
+
+本次提交旨在解决多余副作用的问题。解决方法就是:
+  在每次执行副作用函数A之前，都先将所有的依赖集合中把副作用函数A删除掉。
+  例如，在执行到：
+  setTimeout(() => {
+    proxyObj.isShowText = false;
+  }, 2000);
+  时，就将触发 isShowText 对应的依赖集合中的副作用函数: p!.textContent = proxyObj.isShowText ? proxyObj.text : 'nothing';
+  那么在执行副作用函数之前，将该副作用函数从isShowText和text的依赖集合中删除掉。
+  然后执行副作用函数中，isShowText被读取到，将副作用函数存入isShowText的依赖集合中。但是text未被读取，就不会存到text的依赖集合中了。
+  在执行到：
+  setTimeout(() => {
+    proxyObj.text = '这是修改后的文本.';
+  }, 3000);
+  时，由于依赖集合为空，所以不会执行到副作用函数。
  -->
 <script setup lang="ts">
-type anyFnType = (...arg: any) => any;
+type anyFnType = ((...arg: any) => any) & { deps: (Set<anyFnType> | undefined)[] };
 onMounted(() => {
   // 需要被代理的对象
   const data: Record<string | symbol, any> = {
+    isShowText: true,
     text: '这是初始文本.',
     file: '这是初始文件file.',
   };
-  // 用来存储当前正在执行的函数,避免使用硬编码 setPContent
-  let activeEffect: anyFnType | undefined;
-  function effect(fn: anyFnType) {
-    activeEffect = fn;
-    fn();
-  }
   // 用来存储代理对象发生改变时需要执行的函数
   const bubble = new WeakMap<object, Map<string | symbol, Set<anyFnType>>>();
+  // 用来存储当前正在执行的函数,避免使用硬编码 setPContent
+  let activeEffect: anyFnType | undefined;
+  // 设置副作用函数的函数，本次提交进行改造。
+  function effect(fn: (...arg: any) => any) {
+    // 现在存储到依赖集合的副作用函数改造成了effectFn，可以对effectFn进行改造。
+    const effectFn: anyFnType = function () {
+      // 每次执行真副作用函数之前，都将所有的依赖集合中把副作用函数A删除掉。
+      // 首先遍历activeEffect.deps,将set中的effectFn全部删除
+      effectFn.deps?.forEach((v: Set<anyFnType> | undefined) => {
+        v?.delete(effectFn);
+      });
+      // 然后执行fn
+      fn();
+    };
+    // 用来存储当前副作用函数的依赖集合
+    effectFn.deps = [];
+    activeEffect = effectFn;
+    effectFn();
+    // 这是上一次提交时，修改错了的地方，应该是在执行fn()之后再将activeEffect置为undefined，而不是在get里面
+    activeEffect = undefined;
+  }
   // get时,用来收集当前对象的key的响应函数
   function track(target: Record<string | symbol, any>, key: string | symbol) {
     if (!activeEffect)
@@ -30,8 +64,9 @@ onMounted(() => {
     if (!map?.get(key)) {
       map?.set(key, new Set());
     }
-    map?.get(key)?.add(activeEffect);
-    activeEffect = undefined;
+    const set = map?.get(key);
+    set?.add(activeEffect);
+    activeEffect.deps.push(set); // 将当前副作用函数存入到当前副作用函数的deps中
   }
   // set时,用来触发bubble中的函数
   function trigger(target: Record<string | symbol, any>, key: string | symbol) {
@@ -60,27 +95,43 @@ onMounted(() => {
   const pf = document.getElementById('file');
   if (!p)
     return;
-  function setPContent() {
-    console.log('触发 setPContent...');
-    p!.textContent = proxyObj.text;
-  }
-  function setPFContent() {
-    console.log('触发 setPFContent...');
+  // 相当于模板语法中: <p id="text" > {{proxyObj.isShowText ? proxyObj.text : 'nothing'}} </p>
+  effect(() => {
+    console.log('设置 <p id="text" /> 的值');
+    p!.textContent = proxyObj.isShowText ? proxyObj.text : 'nothing';
+  });
+  effect(() => {
+    console.log('设置  <p id="file" /> 的值');
     pf!.textContent = proxyObj.file;
-  }
-  effect(setPContent);
-  effect(setPFContent);
+  });
+  console.log(bubble);
   setTimeout(() => {
-    proxyObj.text = '这是修改后的文本';
-    proxyObj.file = '这是修改后的文件file';
+    proxyObj.isShowText = false;
+  }, 2000);
+  setTimeout(() => {
+    proxyObj.text = '这是修改后的文本.';
   }, 3000);
 });
+
+// 对比样例
+const reactiveValue = reactive({
+  isShowText: true,
+  text: '这是初始文本.',
+});
+setTimeout(() => {
+  reactiveValue.isShowText = false;
+  // reactiveValue.text = '这是修改后的文本.';
+}, 3000);
 </script>
 
 <template>
   <div>
     <p id="text" />
     <p id="file" />
+    <p>
+      reactiveText:
+      <span>{{ reactiveValue.isShowText ? reactiveValue.text : 'NOTHING' }}</span>
+    </p>
   </div>
 </template>
 
