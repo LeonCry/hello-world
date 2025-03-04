0a705f88866ba7971d002eff07ffc454e0828ee6
343347@libinghai <1395346178@qq.com>
2025-03-04 14:45:02 +0800

    feat: :sparkles: 渲染器:通用化

--- src/views/render/index.vue
+++ src/views/render/index.vue
@@ -1,19 +1,41 @@
 <script setup lang="ts">
+import type { CreateRenderOptionsType, NodeType } from './render';
 import createRenderer from './render';
 
 declare const VueReactivity: {
   effect: (...args: any) => any
   ref: (...args: any) => { value: any }
 };
+// 给createRenderer传入浏览器的一些API,表明这是用于在浏览器中渲染
+const renderFnOptions: CreateRenderOptionsType = {
+  // 创建元素
+  createElement(tag: string) {
+    return document.createElement(tag);
+  },
+  // 设置元素的文本节点
+  setElementText(el: Element, text: string) {
+    el.textContent = text;
+  },
+  // 在指定父元素下,在anchor元素前插入el元素
+  insert(el: Element, parent: Element, anchor: Element | null = null) {
+    parent.insertBefore(el, anchor);
+  },
 
+};
 onMounted(() => {
   const { effect, ref } = VueReactivity;
-  const renderer = createRenderer();
+  const app = document.getElementById('hello')!;
+  const renderer = createRenderer(renderFnOptions);
+  const node: NodeType = {
+    type: 'h1',
+    children: 'hello,world',
+  };
+  renderer.render(node, app);
 });
 </script>
 
 <template>
-  <div id="app" />
+  <div id="hello" />
 </template>
 
 <style scoped lang="scss"></style>
--- src/views/render/render.ts
+++ src/views/render/render.ts
@@ -1,29 +1,40 @@
+// 写在前面: 由于vue.js还具有跨平台的能力,因此,这些和浏览器有关的API都应该抽象化,以便具备多平台渲染的能力.
 export interface NodeType {
   type: string
   children: NodeType | string
 }
+export interface CreateRenderOptionsType {
+  // 创建元素抽象化函数
+  createElement: (tag: string) => HTMLElement
+  // 设置文本节点抽象化函数
+  setElementText: (el: Element, text: string) => void
+  // 插入元素抽象化函数
+  insert: (el: Element, parent: Element, anchor?: Element | null) => void
+}
 // 创建一个渲染器
-function createRenderer() {
+function createRenderer(options: CreateRenderOptionsType) {
+  const { createElement, setElementText, insert } = options;
   // 挂载函数
-  function mountElement(n: NodeType, container: Element) {
+  function mountElement(n: NodeType, container: HTMLElement) {
     // 创建dom元素
-    const el = document.createElement(n.type);
+    const el = createElement(n.type);
     // 如果children是文本节点,则直接设置到textContent里面
-    if (typeof el.children === 'string') {
-      el.textContent = el.children;
+    if (typeof n.children === 'string') {
+      setElementText(el, n.children);
     }
     // 挂载
-    container.appendChild(el);
+    insert(el, container);
   }
-  function patch(n1: NodeType, n2: NodeType, container: Element) {
+  // n1:旧node,n2:新node,container:容器
+  function patch(n1: NodeType | undefined | null, n2: NodeType, container: HTMLElement) {
     // 如果旧的node不存在,说明是挂载
     if (!n1) {
       return mountElement(n2, container);
     }
     return '...';
   }
-  function render(vnode: NodeType, container: Element & { _vnode: NodeType }) {
-    // 有新的node树,新增or更新,挂载
+  function render(vnode: NodeType | null, container: HTMLElement & { _vnode?: NodeType | null }) {
+    // 有新的node树,新增or更新
     if (vnode) {
       patch(container._vnode, vnode, container);
     }
@@ -33,9 +44,6 @@ function createRenderer() {
     }
     container._vnode = vnode;
   }
-  // function hydrate(vnode: any, container: Element) {
-
-  // }
   return { render };
 }
 export default createRenderer;
