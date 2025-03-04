662673cf96a376cac082d4521d7f2d9684583813
343347@libinghai <1395346178@qq.com>
2025-03-04 14:15:32 +0800

    feat: :sparkles: 渲染器: 最简渲染器

--- src/views/render/render.ts
+++ src/views/render/render.ts
@@ -1,6 +1,28 @@
+export interface NodeType {
+  type: string
+  children: NodeType | string
+}
 // 创建一个渲染器
 function createRenderer() {
-  function render(vnode: any, container: Element & { _vnode: any }) {
+  // 挂载函数
+  function mountElement(n: NodeType, container: Element) {
+    // 创建dom元素
+    const el = document.createElement(n.type);
+    // 如果children是文本节点,则直接设置到textContent里面
+    if (typeof el.children === 'string') {
+      el.textContent = el.children;
+    }
+    // 挂载
+    container.appendChild(el);
+  }
+  function patch(n1: NodeType, n2: NodeType, container: Element) {
+    // 如果旧的node不存在,说明是挂载
+    if (!n1) {
+      return mountElement(n2, container);
+    }
+    return '...';
+  }
+  function render(vnode: NodeType, container: Element & { _vnode: NodeType }) {
     // 有新的node树,新增or更新,挂载
     if (vnode) {
       patch(container._vnode, vnode, container);
@@ -15,8 +37,5 @@ function createRenderer() {
 
   // }
   return { render };
-}
-function patch(oldNode: any, newNode: any, container: Element) {
-  console.log(oldNode, newNode, container);
 }
 export default createRenderer;
