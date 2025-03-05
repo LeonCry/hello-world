46ad0fd630e2b20a4dfb0f4a1124e6ba5a83637b
343347@libinghai <1395346178@qq.com>
2025-03-05 18:29:50 +0800

    feat: :sparkles: 渲染器:事件处理

--- src/views/render/index.vue
+++ src/views/render/index.vue
@@ -1,3 +1,4 @@
+<!-- 本次提交用来加入事件的处理 -->
 <script setup lang="ts">
 import type { CreateRenderOptionsType, NodeType } from './render';
 import createRenderer from './render';
@@ -23,7 +24,32 @@ const renderFnOptions: CreateRenderOptionsType = {
   // 进行属性设置,由于在属性设置的时候,我们要优先使用el.key = value 进行设置
   // (why? 因为setAttribute的值总是会被字符串化,导致一些boolean或者其他类型的值不会被正确设置)
   // 另外,还有一些其他特殊的情况,比如说只读时,不能使用el.key = value,只能用setAttribute,因此此类情况需要单独计算
-  patchProps(el: HTMLElement | any, key: string, value: any) {
+  patchProps(el: HTMLElement & { _vei?: any }, key: any, value: any) {
+    // 加入事件的处理,规定以on开头的都是事件处理
+    if (key.startsWith('on')) {
+      const eventName = key.slice(2).toLowerCase();
+      // 用invoker来扮演一个假的事件处理程序
+      const invoker = el._vei || (el._vei = {});
+      // 如果给当前DOM的事件处理函数不为空,即是新加/更新事件处理函数
+      if (value) {
+        // 如果原来的DOM有事件处理函数,则只需要更新下事件处理函数即可
+        if (invoker[key]) {
+          invoker[key] = () => {
+            // value可能有多个响应函数
+            invoker.value.forEach((v: any) => v());
+          };
+          invoker.value = value;
+        }
+        // 如果原先没有,则需要新增事件
+        else {
+          el.addEventListener(eventName, invoker[key]);
+        }
+      }
+      else {
+        invoker[key] && el.removeEventListener(eventName, invoker[key]);
+      }
+      return;
+    }
     function shouldUseSetAttribute() {
       // 例如form
       if (key === 'form')
@@ -33,11 +59,11 @@ const renderFnOptions: CreateRenderOptionsType = {
     }
     if (!shouldUseSetAttribute()) {
       // 例如disabled如果是<input disabled> 那么value = '',因此需要特殊判断
-      if (typeof el[key] === 'boolean' && value === '') {
-        el[key] = true;
+      if (typeof (el as any)[key] === 'boolean' && value === '') {
+        (el as any)[key] = true;
       }
       else {
-        el[key] = value;
+        (el as any)[key] = value;
       }
     }
     else {
@@ -55,7 +81,7 @@ onMounted(() => {
     children: [
       { type: 'h1', children: 'hello,world', props: { id: 1 } },
       { type: 'h2', children: 'hello,world2', props: { style: 'color: red' } },
-      { type: 'h3', children: 'hello,world3' },
+      { type: 'h3', children: 'hello,world3', props: { onClick: [() => console.log(1), () => console.log(2)] } },
     ],
   };
   renderer.render(node, app);
